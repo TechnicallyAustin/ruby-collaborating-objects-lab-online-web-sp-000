f86678121e2a75937e7833fc105411f73cd92df7
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 00:12:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,20 @@
 class Artist 
   @@artists = []
-  def initialize
+  
+  attr_accessor :name 
+  
+  def initalize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@artists
+  end
+  
+  def add_song(song_title)
+    
+    artist.songs 
+  end
+  
+  def songs
+    
