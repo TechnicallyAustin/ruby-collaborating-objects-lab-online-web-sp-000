0cdbc5cba2caf29aaca4751334c3d9c8fdf73977
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 00:39:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,9 @@
 class Song
   attr_accessor :name, :artist
-
+@@all = []
   def initialize(name)
     @name = name
+    save
   end
 
   def self.new_by_filename(file)
@@ -18,4 +19,9 @@ class Song
     self.artist = artist
     artist.add_song(self)
   end
+  
+  def save
+    @@all << self
+  end
+  
 end 
