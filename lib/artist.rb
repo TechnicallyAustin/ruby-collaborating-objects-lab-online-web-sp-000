f2a3fa303832c01443ec8ed1560e17e8dba6a5b4
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 00:18:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   
   def initalize(name)
     @name = name
+    @songs = []
   end
   
   def self.all
@@ -12,9 +13,24 @@ class Artist
   end
   
   def add_song(song_title)
+    artist.songs << song_title 
     
-    artist.songs 
   end
   
   def songs
+    @songs.all
+  end
+  
+  def find_or_create_by_name(artist)
+    artist_selector = @@artists.select?(artist)
+    if artist_selector
+      
+      
+  end
+  
+  def print_songs
+    @songs.each do |song|
+      song
+    end
+  end
     
