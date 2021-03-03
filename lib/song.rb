38d93adbde7307710251c7f9abd9fbe2f83a1111
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 00:37:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,11 @@ def self.all
 end
 
 def artist_name=(name)
-  artist = Artist.find_or_create_by_name(name)
-  self.artist = artist
-  artist.add_song(self)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
 end
 
   def save
