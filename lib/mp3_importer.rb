775275336bbc6544c38e6a9e9308fcf2cf79c4ed
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 00:31:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,10 +6,11 @@ class Mp3Importer
   end
   
   def files
-    
+    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
   end
   
   def new_by_file_name(name)
+    files.each { |file| Song.new_by_file_name(file)
 end
 
   
